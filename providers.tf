815b9ff7ad9a0eff14d33ea33248251d089fc1c7
eilaurencemoughan <laurence.moughan@aerlingus.com>
2024-09-25 16:56:34 +0100

    xc

--- providers.tf
+++ providers.tf
@@ -9,13 +9,12 @@ terraform {
 
 provider "aws" {
   region = "eu-east-1"
-profile = "default"
   # Configuration options
 }
 
 provider "aws" {
   alias = "west2"
   region = "eu-west-2"
-profile = "default"
+
 }
 
